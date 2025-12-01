9b9d9bf5d108d7dadb773deecb4fc6a8f4176344
Pedro Luca Rocha Manera <p.lucarocha@gmail.com>
2025-12-01 00:28:24 -0300

    feat: adiciona a função get_evolucao_mensal para coletar os dados para criar gráfico de evolução mensal

--- services/estatisticas_service.py
+++ services/estatisticas_service.py
@@ -70,3 +70,40 @@ class EstatisticasService:
                 return {"receitas": 0.0, "despesas": 0.0, "saldo": 0.0}
             finally:
                 conn.close()
+                
+    def get_evolucao_mensal(self, usuario_id, limite_meses=12):
+            conn = get_db_connection()
+            cursor = conn.cursor() 
+
+            query = """
+                SELECT 
+                    strftime('%Y-%m', t.data) as mes_ano,
+                    SUM(CASE WHEN c.tipo = 'receita' THEN t.valor ELSE 0 END) as receitas,
+                    SUM(CASE WHEN c.tipo = 'despesa' THEN t.valor ELSE 0 END) as despesas
+                FROM transacao t
+                JOIN categoria c ON t.categoria_id = c.id
+                WHERE t.usuario_id = ?
+                GROUP BY mes_ano
+                ORDER BY mes_ano DESC
+                LIMIT ?
+            """
+            
+            try:
+                cursor.execute(query, (usuario_id, limite_meses))
+                rows = cursor.fetchall()
+                dados_formatados = []
+                for row in rows:
+                    dados_formatados.append({
+                        "mes": row[0],      
+                        "receitas": row[1],
+                        "despesas": row[2],
+                        "saldo": row[1] - row[2]
+                    })
+                
+                return dados_formatados[::-1] # Inverte a lista para ordem cronológica
+                
+            except sqlite3.Error as e:
+                print(f"Erro estatística evolução: {e}")
+                return []
+            finally:
+                conn.close()
