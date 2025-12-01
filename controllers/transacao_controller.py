cc3c57e6707013d211ca544a75038e0ca466bcb1
Pedro Luca Rocha Manera <p.lucarocha@gmail.com>
2025-11-30 23:44:27 -0300

    refactor: introduz os métodos de crud dentro da classe transacaocontroller e cria o setup_routes

--- controllers/transacao_controller.py
+++ controllers/transacao_controller.py
@@ -1,66 +1,71 @@
-from bottle import Bottle, request, response
+from bottle import request, response
 import json
 from services.transacao_service import TransacaoService
+from .base_controller import BaseController
 from utils.auth_middleware import require_auth
 
-transacao_app = Bottle()
-service = TransacaoService()
+class TransacaoController(BaseController):
+    def __init__(self, app):
+        super().__init__(app)
+        self.service = TransacaoService()
+        self.setup_routes()
 
-def json_response(data, status=200): # Helper para formatar respostas JSON
-    response.status = status
-    response.content_type = 'application/json'
-    return json.dumps(data)
+    def _json_response(self, data, status=200):
+        response.status = status
+        response.content_type = 'application/json'
+        return json.dumps(data)
 
-@require_auth  #protejendo as rotas com o decorator 
-@transacao_app.route('/', method='GET') # Listar
-def list_transacoes_by_user():
-    transacoes = service.get_transacao_by_user(request.query.usuario_id)
-    return json_response(transacoes)
+    def setup_routes(self):
+        self.app.route('/transacoes', method='GET', callback=require_auth(self.list_transacoes_by_user))
+        self.app.route('/transacoes', method='POST', callback=require_auth(self.create_transacao))
+        self.app.route('/transacoes/<transacao_id>', method='PUT', callback=require_auth(self.update_transacao))
+        self.app.route('/transacoes/<transacao_id>', method='DELETE', callback=require_auth(self.delete_transacao))
 
-@require_auth  #protejendo as rotas com o decorator 
-@transacao_app.route('/', method='POST') # Criar
-def create_transacao(): 
-    data = request.json
-    if not data or 'valor' not in data or 'usuario_id' not in data or 'categoria_id' not in data:
-        return json_response({'error': 'Dados incompletos para criar a transação.'}, status=400)
-    
-    new_id = service.create_transacao(
-        usuario_id=data['usuario_id'],
-        categoria_id=data['categoria_id'],
-        valor=data['valor'],
-        data=data['data'],
-        descricao=data.get('descricao', '')
-    )
+    def list_transacoes_by_user(self):
+        transacoes = self.service.get_transacao_by_user(request.user_id)
+        return self._json_response(transacoes)
 
-    if new_id:
-        return json_response({'message': 'Transação criada com sucesso.', 'transacao_id': new_id}, status=201)
-    else:
-        return json_response({'error': 'Ocorreu um erro ao criar a transação.'}, status=500)
+    def create_transacao(self):
+        data = request.json
+        if not data or 'valor' not in data or 'categoria_id' not in data:
+            return self._json_response({'error': 'Dados incompletos para criar a transação.'}, status=400)
+        
+        new_id = self.service.create_transacao(
+            usuario_id=request.user_id,
+            categoria_id=data['categoria_id'],
+            valor=data['valor'],
+            data=data['data'],
+            descricao=data.get('descricao', '')
+        )
 
-@require_auth    #protejendo as rotas com o decorator 
-@transacao_app.route('/<transacao_id>', method='PUT') # Atualizar
-def update_transacao(transacao_id):
-    data = request.json
-    if not data:
-        return json_response({'error': 'Dados incompletos para atualizar a transação.'}, status=400)
-    success = service.update_transacao(
-        transacao_id=transacao_id,
-        usuario_id=data.get('user_id'),
-        valor=data.get('valor'),        
-        data=data.get('data'),
-        descricao=data.get('descricao'),
-        categoria_id=data.get('categoria_id') 
-    )
-    if success:
-        return json_response({'message': "Transação atualizada com sucesso."})
-    else:
-        return json_response({'error': 'Ocorreu um erro ao atualizar a transação.'}, status=500)
+        if new_id:
+            return self._json_response({'message': 'Transação criada com sucesso.', 'transacao_id': new_id}, status=201)
+        else:
+            return self._json_response({'error': 'Ocorreu um erro ao criar a transação.'}, status=500)
 
-@require_auth   #protejendo as rotas com o decorator 
-@transacao_app.route('/<transacao_id>',  method='DELETE') # Deletar
-def delete_transacao(transacao_id):
-    success = service.delete_transacao(transacao_id, request.get('user_id'))
-    if success:
-        return json_response({'message': 'Transação deletada com sucesso.'})
-    else:
-        return json_response({'error': 'Ocorreu um erro ao deletar a transação.'}, status=500)
+    def update_transacao(self, transacao_id):
+        data = request.json
+        if not data:
+            return self._json_response({'error': 'Dados incompletos para atualizar a transação.'}, status=400)
+        
+        success = self.service.update_transacao(
+            transacao_id=transacao_id,
+            usuario_id=request.user_id,
+            valor=data.get('valor'),        
+            data=data.get('data'),
+            descricao=data.get('descricao'),
+            categoria_id=data.get('categoria_id') 
+        )
+        
+        if success:
+            return self._json_response({'message': "Transação atualizada com sucesso."})
+        else:
+            return self._json_response({'error': 'Ocorreu um erro ao atualizar a transação.'}, status=500)
+
+    def delete_transacao(self, transacao_id):
+        success = self.service.delete_transacao(transacao_id, request.user_id)
+        
+        if success:
+            return self._json_response({'message': 'Transação deletada com sucesso.'})
+        else:
+            return self._json_response({'error': 'Ocorreu um erro ao deletar a transação.'}, status=500)
