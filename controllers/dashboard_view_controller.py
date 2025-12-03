fdf86a6ed3c8680cdb89a8750942f68e3b848492
Pedro Luca Rocha Manera <p.lucarocha@gmail.com>
2025-12-03 00:40:30 -0300

    fix: resolve problema em imprimir mensagem sobre recorrência lançada

--- controllers/dashboard_view_controller.py
+++ controllers/dashboard_view_controller.py
@@ -1,12 +1,14 @@
-from bottle import request, redirect
+from bottle import request, redirect, template
 from .base_controller import BaseController
 from utils.auth_middleware import require_auth 
 from services.recorrencia_service import RecorrenciaService
+from services.transacao_service import TransacaoService
 
 class DashboardViewController(BaseController):
     def __init__(self, app):
         super().__init__(app)
         self.recorrencia_service = RecorrenciaService()
+        self.transacao_service = TransacaoService()
         self.setup_routes()
 
     def setup_routes(self):
@@ -16,12 +18,14 @@ class DashboardViewController(BaseController):
     def view_dashboard(self):
         user_id = request.user_id # @require_auth garante que o request_user.id existe
 
-        #verifica algo atrasado
-        novas_transacoes = self.recorrencia_service.processar_recorrencia(user_id)
-        mensagem = None
-        if novas_transacoes > 0 :
-            mensagem = f"{novas_transacoes} transações recorrentes foram lançadas automaticamente!"
-            #aqui busca o saldo real
-            
-        # Renderiza o HTML e passa o nome do usuário logado
-        return self.render('views/dashboard.html', user_name=request.user_name, msg=mensagem)
+        lista_transacoes = self.transacao_service.get_transacao_by_user(user_id)
+        lista_recorrencias = self.recorrencia_service.processar_recorrencia(user_id)
+
+
+        return template(
+            'views/dashboard',  
+            transacoes=lista_transacoes,
+            recorrencias=lista_recorrencias,
+            user_name=request.user_name, 
+            msg = f"{lista_recorrencias} Transações recorrentes foram processadas automaticamente." if lista_recorrencias > 0 else None
+        )
